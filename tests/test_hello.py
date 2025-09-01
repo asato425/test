3aa57761fee2ccfe68198917fe3886e7b332f020
asato425 <suasa425@icloud.com>
2025-09-01 19:11:50 +0900

    test commit

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,7 +3,7 @@ from test.hello import hello
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello("WorId"), "Hello, World!")
+        self.assertEqual(hello("W0rld"), "Hello, World!")
         self.assertEqual(hello("テスト"), "Hello, テスト!")
 
 if __name__ == "__main__":
